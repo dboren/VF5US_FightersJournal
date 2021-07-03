c9431d61a3568c28f4ce27fa4eba367c9bedee7c
dboren <danielboren@att.net>
2021-07-03 14:21:33 -0500

    added character model

---
+++ models/character.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Character extends Model {}
+
+Character.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        fighting_style: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        weight_class: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        portrait: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'character',
+    }
+);
+
+module.exports = Character;
