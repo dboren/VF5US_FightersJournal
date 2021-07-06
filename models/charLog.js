e61ac73b40cc180fe35f9d67334f72bef7b53330
dboren <danielboren@att.net>
2021-07-06 15:02:28 -0500

    added character log model

---
+++ models/charLog.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const Character = require('./character');
+
+class CharacterLog extends Model {}
+
+CharacterLog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoincrement: true,
+        },
+        date_added: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        hours_played: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        character_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'character',
+                key: 'id',
+            },
+        },
+        characterName: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'character',
+                key: 'name',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'charLog',
+    }
+);
+
+module.exports = CharacterLog;
