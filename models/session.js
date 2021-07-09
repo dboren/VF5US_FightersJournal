3dbaaa27463d7e92bd5553264cd0cc11626c4d3c
dboren <danielboren@att.net>
2021-07-08 19:56:14 -0500

    added session model

---
+++ models/session.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Session extends Model {}
+
+Session.init(
+    {
+        id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    duration: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    activity: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    log_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'charLog',
+            key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'session',
+    }    
+);
+
+module.exports = Session;
+
