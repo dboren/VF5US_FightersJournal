ed7fb4dc1ce66ba3072ce0e84edb796b4b88da5f
dboren <danielboren@att.net>
2021-07-08 19:45:24 -0500

    added combo model

---
+++ models/combo.js
@@ -0,0 +1,62 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Combo extends Model {}
+
+Combo.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        ch_only: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+        requires_wall: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        input: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        hitzone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        startup: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        damage: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        targets: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        misc_notes: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        log_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'charLog',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'combo',
+    },
+);
+
+module.exports = Combo;
