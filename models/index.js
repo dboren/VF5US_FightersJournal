e2cb0f0df1be765858134c944fc3217feb592190
dboren <danielboren@att.net>
2021-07-13 21:36:50 -0500

    created index model

--- models/index.js
+++ models/index.js
@@ -1,1 +1,41 @@
-// const character = require('.')
+const Character = require('./character');
+const CharLog = require('./charLog');
+const Combo = require('./combo');
+const Note = require('./note');
+const { hasMany } = require('./session');
+const Session = require('./session');
+const User = require('./user');
+
+CharLog.hasMany(Note, {
+    foreignKey: 'log_id',
+});
+
+Note.belongsTo(CharLog, {
+    foreignKey: 'log_id',
+});
+
+CharLog.hasMany(Combo, {
+    foreignKey: 'log_id',
+});
+
+Combo.belongsTo(CharLog, {
+    foreignKey: 'log_id',
+});
+
+CharLog.hasMany(Session, {
+    foreignKey: 'log_id',
+});
+
+Session.belongsTo(CharLog, {
+    foreignKey: 'log_id',
+});
+
+User.hasMany(CharLog, {
+    foreignKey: 'user_id',
+});
+
+CharLog.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = {Character, CharLog, Combo, Note, Session, User}
