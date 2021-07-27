e596df8fa143dbe00bca02d5bc9065f2ac9f5f77
dboren <danielboren@att.net>
2021-07-27 15:32:31 -0500

    corrected server file to properly import from connection.js. Added basic routing template and routes to get all characters and create a new user. Verified that app connects to database and tested both routes successfully with Insomnia

---
+++ routes/api/characterRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { Character } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const characterData = await Character.findAll();
+
+        res.status(200).json(characterData);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const path = require('path');
 const express = require("express");
 
-const sequelize = require("sequelize");
+const sequelize = require('./config/connection');
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,8 +13,12 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log("Now listening"));
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
