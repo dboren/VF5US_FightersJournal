7538e98a226fe1d26944c2a9fc2418541ae56018
dboren <danielboren@att.net>
2021-08-18 21:58:02 -0500

    replaced incorrect argument in try block of notes post route

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -6,7 +6,7 @@ router.post('/', async (req, res) => {
 
         const noteData = await Note.create(req.body);
 
-        res.status(200).json(logData);
+        res.status(200).json(noteData);
 
     } catch (err) {
         res.status(400).json(err);
