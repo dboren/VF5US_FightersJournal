fcea4b2c7cd6f93c0705a51a9a387825f6006fdd
dboren <danielboren@att.net>
2021-11-02 15:52:08 -0500

    added route to get single note by id

--- routes/api/characterRoutes.js
+++ routes/api/characterRoutes.js
@@ -12,4 +12,20 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+      const characterData = await Character.findByPK(req.params.id);
+      
+  
+      if (!characterData) {
+        res.status(404).json({ message: "No character found" });
+        return;
+      }
+  
+      res.status(200).json(characterData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Note } = require('../../models');
+const { Note, Log } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
@@ -10,6 +10,35 @@ router.post('/', async (req, res) => {
 
     } catch (err) {
         res.status(400).json(err);
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const noteData = await Note.findAll();
+
+        res.status(200).json(noteData);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const dbNoteData = await Note.findByPk(req.params.id, {
+            include: [{ model: Log }],
+        });
+
+        res.status(200).json(dbNoteData);
+
+        if(!dbNoteData) {
+            res.status(404).json({message: 'No notes with this id'});
+        }
+
+
+    } catch(err) {
+        res.status(500).json(err);
     }
 });
 
