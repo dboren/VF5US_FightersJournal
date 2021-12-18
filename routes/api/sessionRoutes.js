32d02fc89cebcdb0258209ad9b52b080669448f9
dboren <danielboren@att.net>
2021-12-18 17:07:01 -0600

    added route to get session by ID

--- routes/api/sessionRoutes.js
+++ routes/api/sessionRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Session } = require('../../models');
+const { Session, Log } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
@@ -23,5 +23,22 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const sessionData = await Session.findByPk(req.params.id, {
+            include: [{ model: Log }],
+        });
+
+        res.status(200).json(sessionData);
+
+        if(!sessionData) {
+            res.status(404).json({message: 'No sessions with this id'});
+        }
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
