1450928d9f5f1f72b1bae874a831f68bbbadcc6b
Daniel Boren <danielboren52@gmail.com>
2022-08-21 11:38:57 -0500

    added log seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,14 @@
+use vf5usfj_db;
+
 INSERT INTO user
     (username, email, password)
 VALUES
     ('JudgementKazzy', 'kkiryu@test.com', 'sunflower'),
     ('MadDog', 'gmajima@test.com', 'kiryuchan');
+
+
+INSERT INTO log
+    (character_name, hours_played, user_id),
+VALUES
+    ('Akira Yuki', 3, 1),
+    ('Kage-Maru', 1, 2)
